65b30ab6151fa7eefd483db4232754b1793f4aa6
Paul Mauviel <p.a.mauviel@gmail.com>
2021-10-19 22:57:29 -0400

    commit the template

---
+++ README.md
@@ -0,0 +1,5 @@
+# LearnOpenGL CMake Project template
+
+Uses submodules to bring in GLFW so be sure to clone with
+
+`git clone --recursive https://github.com/mauville-technologies/learnopengl-cmake-template`
---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD!" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
